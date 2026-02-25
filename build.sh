50ea0fe906e0b6efc7e1aa6d564fc3db08665177
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-02-25 23:45:24 +0530

    done

--- build.sh
+++ build.sh
@@ -10,3 +10,17 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 
+# Auto-create superuser if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+from apps.accounts.models import Profile
+User = get_user_model()
+if not User.objects.filter(username='oppie_549').exists():
+    u = User.objects.create_superuser('oppie_549', 'omprakashbandi583@gmail.com', 'Nancy123abc@')
+    p = Profile.objects.get(user=u)
+    p.role = 'ADMIN'
+    p.save()
+    print('Superuser oppie_549 created with ADMIN role')
+else:
+    print('Superuser oppie_549 already exists')
+"
